5a7b3765e02ae2cf80974f240fe092ff51a898fb
Artem Shevtsov <shevtsov.ae@edu.spbstu.ru>
2023-10-25 14:40:13 +0300

    Fix:Task 4

--- src/main/java/org/shevtsov/fourthTask/ArifmeticalProgression.java
+++ src/main/java/org/shevtsov/fourthTask/ArifmeticalProgression.java
@@ -4,17 +4,18 @@ import java.util.Scanner;
 
 public class ArifmeticalProgression {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int firstElement = scanner.nextInt();
-        int difference = scanner.nextInt();
-        int numberOfElements = scanner.nextInt();
 
-        long sum = calculateSum(firstElement, difference, numberOfElements);
+
+        long sum = calculateSum(args);
 
         System.out.println("Сумма арифметической прогрессии: " + sum);
     }
 
-    public static long calculateSum(int firstElement, int difference, int numberOfElements) {
+    public static long calculateSum(String [] args) {
+        int firstElement=Integer.valueOf(args[0]);
+        int difference=Integer.valueOf(args[1]);
+        int numberOfElements=Integer.valueOf(args[2]);
+
         long sum = 0;
 
         for (int i = 0; i < numberOfElements; i++) {
